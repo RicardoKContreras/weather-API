59d4bb5a4ee464050a066ce71d747b58547412b7
Jimbei <contrerasr2272@gmail.com>
2022-10-09 07:15:41 -0400

    setting up weather page

--- assets/css/js/hompage.js
+++ assets/css/js/hompage.js
@@ -1,82 +1,50 @@
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
-var cityFormEl = document.querySelector("#city-form");
-var cityInputEl = document.querySelector("#cityname");
-var displayRepos = function(data, cityname) {
-  console.log(data);
-  console.log(cityname);
-  repoContainerEl.textContent = "";
-repoSearchTerm.textContent = cityname;
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+//executed upon a form submission browser event.
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var cityname = nameInputEl.value.trim();
+
+if (cityname) {
+  getGeoLocation(cityname);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a city");
+}
+//to see if put fires
+  // console.log(event);
 };
 
-
-//*Note how we've added a parameter to the getUserRepos() function and inserted the parameter into the GitHub API URL. We then use the newly formatted URL in the subsequent fetch() request.
-// var getCitycordinates = function(city) {
-// var apiKey = "be12cc98c8a80e10300f59755d0a1bbc";
-//  var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
- 
-  
-//   fetch(geoLocation).then(function(response) {
-//     response.json().then(function(data) {
-//       displayRepos(data, city);
-//     });
-//   });
-// }
-
-var getCitycordinates = function(city) {
+var getGeoLocation = function(city) {
   // format the github api url
-   var apiKey = "be12cc98c8a80e10300f59755d0a1bbc";
- var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
+     var apiKey = "be12cc98c8a80e10300f59755d0a1bbc";
+  var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
 
   // make a request to the url
   fetch(geoLocation).then(function(response) {
     response.json().then(function(data) {
-      var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=" + apiKey;
-      console.log(city);
-          fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+      console.log(data);
+      var cityLat = data[0].lat;
+      var cityLon = data[0].lon;
+      console.log(cityLat);
+      console.log(cityLon);
+      getWeatherInfo(cityLat, cityLon);
     });
-    }); 
   });
 };
 
-
-// var getCityRepos = function(lat, lon) {
-//   var apiKey = "be12cc98c8a80e10300f59755d0a1bbc";
-//       // format the github api url
-    
-//     // make a request to the url
-//     fetch(apiUrl).then(function(response) {
-//       response.json().then(function(data) {
-//         console.log(data);
-//       });
-//     });
-//     // console.log(apiUrl);
-//     // fetch(apiUrl).then(function(response) {
-//     //   response.json().then(function(data) {
-//     //     getCitycordinates(data, data.dt);
-//     //   });
-//     // });
-//   }
-
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-      // get value from input element
-var cityname = cityInputEl.value.trim();
-
-if (cityname) {
-  getCitycordinates(cityname);
-  
-  cityInputEl.value = "";
-} else {
-  alert("Please enter a city!");
+var getWeatherInfo = function(lat, lon) {
+  var apiKey = "be12cc98c8a80e10300f59755d0a1bbc";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+console.log(apiUrl);
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+      console.log(apiUrl);
+    });
+  });
 }
-  }
 
-
-//   };
-  
-//   getUserRepos("microsoft");
-  cityFormEl.addEventListener("submit", formSubmitHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);
